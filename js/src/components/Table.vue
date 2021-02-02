513b8eb90f7308fb9750b30bcef31f3ce788e850
NVMSW <78442308+NVMSW@users.noreply.github.com>
2021-02-02 22:44:18 +0200

    custom style removed

--- js/src/components/Table.vue
+++ js/src/components/Table.vue
@@ -1,5 +1,5 @@
 <template>
-  <table>
+  <table >
     <tr>
       <th>ID</th>
       <th>Title</th>
@@ -70,33 +70,7 @@ export default {
 
 <style>
 .highlight {
-  background: blue;
-}
-table {
-  font-family: Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
+  background: rgb(90, 159, 223);
 }
 
-table td,
-table th {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
-
-table tr:nth-child(even) {
-  background-color: #f2f2f2;
-}
-
-table tr:hover {
-  background-color: #ddd;
-}
-
-table th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #4CAF50;
-  color: white;
-}
 </style>
